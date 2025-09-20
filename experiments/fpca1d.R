9b693a3154a660f513ff0955565f11a3438fa86f
mynanshan <muye.nanshan@gmail.com>
2025-09-19 18:59:13 -0700

    Simplify fpca1d

--- experiments/fpca1d.R
+++ experiments/fpca1d.R
@@ -31,7 +31,8 @@ source("./R/onlineFDAlocalpoly.R")
 source("./R/fpcaReg.R")
 source("./data_generation/generator.R")
 
-noiseList <- c(0.1, 0.5, 1.)
+# noiseList <- c(0.1, 0.5, 1.)
+noiseList <- c(0.5)
 nBatch <- 5
 nParams <- 6
 nPass <- 3
