e13493975a609f74338b52901fd1554777c81f8e
mynanshan <muye.nanshan@gmail.com>
2025-10-26 19:17:37 -0700

    Fix stepsize0 bug in fpca1d.R

--- experiments/fpca1d.R
+++ experiments/fpca1d.R
@@ -206,8 +206,6 @@ for (noise_sd in noiseList) {
   )$grad_Theta
   grad_Theta_init <- manifold.Stiefel.project(grad_Theta_init, ThetaInit, G)
   g_Theta_init_norm <- sqrt(sum(grad_Theta_init * G %*% grad_Theta_init) / q)
-  sgd_lr0 <- stepsize0 / g_Theta_init_norm
-  message("> Step size = ", stepsize0)
 
   inits <- list(
     Theta = ThetaInit,
@@ -215,146 +213,148 @@ for (noise_sd in noiseList) {
     sigma2 = sigma2Init
   )
 
-  for (sgdtype in c("sgd", "adagrad", "adam")) {
-    message(">>> sgdtype = ", sgdtype)
+  for (stepsize0 in stepsizeList) {
+    sgd_lr0 <- stepsize0 / g_Theta_init_norm
+    message("> Step size = ", stepsize0)
 
-    for (stepsize0 in stepsizeList) {
+    for (sgdtype in c("sgd", "adagrad", "adam")) {
+      message(">>> sgdtype = ", sgdtype)
 
-    fit <- fpca.sgd(
-      fdata_generator,
-      tgrid,
-      inits = inits,
-      meanfun = FALSE,
-      tau = NULL,
-      tau.control = list(
-        ntau = nParams,
-        nselect = 2,
-        maxtau = 1e-1,
-        mintau = 1e-6
-      ),
-      nbatch = nBatch,
-      maxIter = nPass * nIter1pass,
-      stepsize = ifelse(sgdtype %in% c("sgd", "sgdm"), sgd_lr0, stepsize0),
-      nIter.constStepSize = 0,
-      stepsize.decayrate = 0.51,
-      stepsize.min = stepsize.min,
-      # period.decay = 5 * nBlockIter,
-      period.decay = 2 * nBlockIter,
-      # period.decay = 1,
-      nIter.slowerdecay = nIter1pass,
-      # stepsize.decayrate.slow = 0.5,
-      stepsize.decayrate.slow = 0.25,
-      dynlr = ifelse(sgdtype %in% c("sgd", "sgdm"), TRUE, FALSE),
-      dynlrCtrl = list(
-        niter = 20 * nBlockIter,
-        reset = 5 * nBlockIter,
-        refdn = stepsize0,
-        w = 0.9
-      ),
-      sgdtype = sgdtype,
-      adamw = TRUE,
-      adam.rescale = TRUE,
-      # ada.start = 20 * nBlockIter + 1,
-      ada.start = 25 * nBlockIter + 1,
-      # ada.start = 1,
-      # adareset = 10 * nBlockIter,
-      adareset = 20 * nBlockIter,
-      adareset.end = nIter1pass,
-      # adareset.end = 10 * nBlockIter,
-      asgd.start = 10 * nBlockIter + 1,
-      asgd.reset = 40 * nBlockIter,
-      asgd.reset.end = nIter1pass,
-      asgd.end = Inf,
-      nIter.1stTune = nRoundNoTune * nBlockIter,
-      nIter.lastTune = nIter1pass,
-      nIter.tauNoIncrease = floor(0.3 * nIter1pass),
-      period.tune = nBlockIter,
-      period.record = nBlockIter,
-      verbose = TRUE
-    )
-
-    check <- fit$check
-
-    tau.min <- fit$tau.min
-    l <- which(fit$tau == tau.min)[1]
-    Theta <- fit$Theta[,, l]
-    lambda <- fit$lambda[, l]
-    sigma2 <- fit$sigma2[l]
-    Theta.avg <- fit$Theta.avg[,, l]
-    lambda.avg <- fit$lambda.avg[, l]
-    sigma2.avg <- fit$sigma2.avg[l]
-
-    params <- fit$params.history$params
-    vcrits <- fit$vcrit.history
-
-    # check eigenfunctions
-    PhiEstEval <- eval_fd(evalGrid, FuncData(Theta, basis))
-    PhiEstEval <- match_fpc(PhiEstEval, PhiTrueEval[, 1:q, drop = F])
-    params$Theta <- params$Theta[,
-      attributes(PhiEstEval)$match_id, , , drop = F ]
-    params$Theta[, attributes(PhiEstEval)$flipped, , ] <-
-      -params$Theta[, attributes(PhiEstEval)$flipped, , ]
-    PhiAvgEval <- eval_fd(evalGrid, FuncData(Theta.avg, basis))
-    PhiAvgEval <- match_fpc(PhiAvgEval, PhiTrueEval[, 1:q, drop = F])
-    params$Theta.avg <- params$Theta.avg[,
-      attributes(PhiAvgEval)$match_id, , , drop = F ]
-    params$Theta.avg[, attributes(PhiAvgEval)$flipped, , ] <-
-      -params$Theta.avg[, attributes(PhiAvgEval)$flipped, , ]
-
-    tau_path <- with(
-      fit$tau.select,
-      extract_tau_path(tau.history, tau.selectId, l)
-    )
-    tau_path_id <- c(
-      tau_path$tau_path_id,
-      rep(1, (nPass - 1) * round(N / nBlock))
-    )
-    tau_path_id_extend <- c(rep(tau_path_id[1], nRoundNoTune), tau_path_id)
-
-    ThetaAll <- sapply(
-      seq_along(fit$params.history$iter.params),
-      \(i) params$Theta[,, tau_path_id_extend[i], i],
-      simplify = "array"
-    )
-    rmseAll <- rmse_phi(ThetaAll, phiTrueFunc$coefs, B)
-    ThetaAll.avg <- sapply(
-      seq_along(fit$params.history$iter.params),
-      \(i) params$Theta.avg[,, tau_path_id_extend[i], i],
-      simplify = "array"
-    )
-    rmseAll.avg <- rmse_phi(ThetaAll.avg, phiTrueFunc$coefs, B)
-
-    times <- c(
-      colSums(fit$time.history[, 1:nIter1pass]),
-      fit$time.history[1, (nIter1pass + 1):(nPass * nIter1pass)]
-    )
-    times <- colSums(matrix(times, nrow = nBlockIter))
-    times <- c(difftime(end_time.init, start_time.init, units = 'secs'), times)
-
-    res <- rbind(
-      res,
-      data.frame(
-        noise = noise_sd,
-        seed = rep(seed, nRecord + 1),
-        Method = rep(paste0("OnlineFPCA-", sgdtype), nRecord + 1),
-        StepSize = rep(stepsize0, nRecord + 1),
-        N = c(0, nIters),
-        Ninit = rep(Ninit, nRecord + 1),
-        initMethod = rep(initMethod, nRecord + 1),
-        npc = rep(q, nRecord + 1),
-        nBatch = rep(nBatch, nRecord + 1),
-        Time = times,
-        RMSEphi1 = rmseAll[, 1],
-        RMSEphi2 = rmseAll[, 2],
-        RMSEphi3 = rmseAll[, 3],
-        RMSEphi1.avg = rmseAll.avg[, 1],
-        RMSEphi2.avg = rmseAll.avg[, 2],
-        RMSEphi3.avg = rmseAll.avg[, 3]
+      fit <- fpca.sgd(
+        fdata_generator,
+        tgrid,
+        inits = inits,
+        meanfun = FALSE,
+        tau = NULL,
+        tau.control = list(
+          ntau = nParams,
+          nselect = 2,
+          maxtau = 1e-1,
+          mintau = 1e-6
+        ),
+        nbatch = nBatch,
+        maxIter = nPass * nIter1pass,
+        stepsize = ifelse(sgdtype %in% c("sgd", "sgdm"), sgd_lr0, stepsize0),
+        nIter.constStepSize = 0,
+        stepsize.decayrate = 0.51,
+        stepsize.min = stepsize.min,
+        # period.decay = 5 * nBlockIter,
+        period.decay = 2 * nBlockIter,
+        # period.decay = 1,
+        nIter.slowerdecay = nIter1pass,
+        # stepsize.decayrate.slow = 0.5,
+        stepsize.decayrate.slow = 0.25,
+        dynlr = ifelse(sgdtype %in% c("sgd", "sgdm"), TRUE, FALSE),
+        dynlrCtrl = list(
+          niter = 20 * nBlockIter,
+          reset = 5 * nBlockIter,
+          refdn = stepsize0,
+          w = 0.9
+        ),
+        sgdtype = sgdtype,
+        adamw = TRUE,
+        adam.rescale = TRUE,
+        # ada.start = 20 * nBlockIter + 1,
+        ada.start = 25 * nBlockIter + 1,
+        # ada.start = 1,
+        # adareset = 10 * nBlockIter,
+        adareset = 20 * nBlockIter,
+        adareset.end = nIter1pass,
+        # adareset.end = 10 * nBlockIter,
+        asgd.start = 10 * nBlockIter + 1,
+        asgd.reset = 40 * nBlockIter,
+        asgd.reset.end = nIter1pass,
+        asgd.end = Inf,
+        nIter.1stTune = nRoundNoTune * nBlockIter,
+        nIter.lastTune = nIter1pass,
+        nIter.tauNoIncrease = floor(0.3 * nIter1pass),
+        period.tune = nBlockIter,
+        period.record = nBlockIter,
+        verbose = TRUE
       )
-    )
 
-    } # end of all step sizes
-  }  # end of all sgdtype
+      check <- fit$check
+
+      tau.min <- fit$tau.min
+      l <- which(fit$tau == tau.min)[1]
+      Theta <- fit$Theta[,, l]
+      lambda <- fit$lambda[, l]
+      sigma2 <- fit$sigma2[l]
+      Theta.avg <- fit$Theta.avg[,, l]
+      lambda.avg <- fit$lambda.avg[, l]
+      sigma2.avg <- fit$sigma2.avg[l]
+
+      params <- fit$params.history$params
+      vcrits <- fit$vcrit.history
+
+      # check eigenfunctions
+      PhiEstEval <- eval_fd(evalGrid, FuncData(Theta, basis))
+      PhiEstEval <- match_fpc(PhiEstEval, PhiTrueEval[, 1:q, drop = F])
+      params$Theta <- params$Theta[,
+        attributes(PhiEstEval)$match_id, , , drop = F ]
+      params$Theta[, attributes(PhiEstEval)$flipped, , ] <-
+        -params$Theta[, attributes(PhiEstEval)$flipped, , ]
+      PhiAvgEval <- eval_fd(evalGrid, FuncData(Theta.avg, basis))
+      PhiAvgEval <- match_fpc(PhiAvgEval, PhiTrueEval[, 1:q, drop = F])
+      params$Theta.avg <- params$Theta.avg[,
+        attributes(PhiAvgEval)$match_id, , , drop = F ]
+      params$Theta.avg[, attributes(PhiAvgEval)$flipped, , ] <-
+        -params$Theta.avg[, attributes(PhiAvgEval)$flipped, , ]
+
+      tau_path <- with(
+        fit$tau.select,
+        extract_tau_path(tau.history, tau.selectId, l)
+      )
+      tau_path_id <- c(
+        tau_path$tau_path_id,
+        rep(1, (nPass - 1) * round(N / nBlock))
+      )
+      tau_path_id_extend <- c(rep(tau_path_id[1], nRoundNoTune), tau_path_id)
+
+      ThetaAll <- sapply(
+        seq_along(fit$params.history$iter.params),
+        \(i) params$Theta[,, tau_path_id_extend[i], i],
+        simplify = "array"
+      )
+      rmseAll <- rmse_phi(ThetaAll, phiTrueFunc$coefs, B)
+      ThetaAll.avg <- sapply(
+        seq_along(fit$params.history$iter.params),
+        \(i) params$Theta.avg[,, tau_path_id_extend[i], i],
+        simplify = "array"
+      )
+      rmseAll.avg <- rmse_phi(ThetaAll.avg, phiTrueFunc$coefs, B)
+
+      times <- c(
+        colSums(fit$time.history[, 1:nIter1pass]),
+        fit$time.history[1, (nIter1pass + 1):(nPass * nIter1pass)]
+      )
+      times <- colSums(matrix(times, nrow = nBlockIter))
+      times <- c(difftime(end_time.init, start_time.init, units = 'secs'), times)
+
+      res <- rbind(
+        res,
+        data.frame(
+          noise = noise_sd,
+          seed = rep(seed, nRecord + 1),
+          Method = rep(paste0("OnlineFPCA-", sgdtype), nRecord + 1),
+          StepSize = rep(stepsize0, nRecord + 1),
+          N = c(0, nIters),
+          Ninit = rep(Ninit, nRecord + 1),
+          initMethod = rep(initMethod, nRecord + 1),
+          npc = rep(q, nRecord + 1),
+          nBatch = rep(nBatch, nRecord + 1),
+          Time = times,
+          RMSEphi1 = rmseAll[, 1],
+          RMSEphi2 = rmseAll[, 2],
+          RMSEphi3 = rmseAll[, 3],
+          RMSEphi1.avg = rmseAll.avg[, 1],
+          RMSEphi2.avg = rmseAll.avg[, 2],
+          RMSEphi3.avg = rmseAll.avg[, 3]
+        )
+      )
+
+    }  # end of all sgdtype
+  } # end of all step sizes
 
   if (compare) {
 
