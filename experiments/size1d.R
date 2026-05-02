b93db7a3022b79cc30f157494e9086f38f1c5fe3
mynanshan <muye.nanshan@gmail.com>
2026-05-01 19:43:21 -0700

    Change mem cpu setting

--- experiments/size1d.R
+++ experiments/size1d.R
@@ -4,8 +4,8 @@
 #SBATCH --time=12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=4G
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=8G
 
 parser <- argparse::ArgumentParser(
   description = "Determine the settings for this simple simulation."
