d3bcf2d725cd3d629559320f43a55dd6b4e171a8
mynanshan <muye.nanshan@gmail.com>
2026-05-10 08:38:51 -0700

    Lower max-obs limit for mOpCov

--- experiments/size2d.R
+++ experiments/size2d.R
@@ -50,7 +50,7 @@ parser$add_argument(
 parser$add_argument(
   "--mopcov-max-obs",
   type = "integer",
-  default = 50000,
+  default = 30000,
   help = "Largest number of total observations allowed for mOpCov. Use <=0 to disable this guard."
 )
 args <- parser$parse_args()
